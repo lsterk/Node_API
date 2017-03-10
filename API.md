288ee67bba1a09e9a569394661b8e49cc642b2ae
Landon Sterk <landonsterk@gmail.com>
2017-03-10 16:08:02 -0500

    Updated API Docs Also added database connection

--- API.md
+++ API.md
@@ -5,37 +5,110 @@
 
 ## Public Actions
 
-GET /test
+### GET /test
 
 Testing method to determine current server status
 
-POST /login/:uID
+Request: no formatting needed
 
-submission example:
+Reply:
+
+```html
+Test server running
+```
+
+### POST /login/:uID
+
+To receive an Access Token for a particular user, upload a JSON-style body with
+an API Key and the plain-text PIN of the user:
+
+Request:
+```javascript
 {
-  "PIN" : hash(1453)  
+  "apiKey" : String ,
+  "PIN" : String
 }
+```
+
+Response:
+
+```javascript
+HTTP Status Code: 201
+{
+  "uID" : String ,
+  "accessToken" : String  
+}
+```
+
+Some submissions will generate errors, according to incorrect access privileges, etc. See the table below for examples:
+
+| HTTP Status Code | Status Explanation
+| :---------------:  | :---------------- |
+| 201 | AccessToken created, and sent back in body|
+|400 | API Key or PIN not included |
+| 401 | PIN was not successful in authenticating; API Key is not valid. Server will reply with JSON
+
+
+#### Successful submission example
+request example:
+
+```javascript
+{
+  "apiKey" : UUIDV1,
+  "PIN" : "1453"
+}
+```
 
 reply example:
 
+```javascript
+HTTP Status Code: 201
 {
   "uID" : "1403378",
-  "accessToken" :
+  "accessToken" : UUIDv1()
 }
+
+```
+#### Failed submission example
+request example:
+
+```javascript
+{
+  "apiKey" : "a valid key",
+  "pin" : "wrong pin"
+}
+```
+
+reply example:
+
+```javascript
+HTTP Status Code: 401
+{
+  "errorCode" : 401,
+  "errorMsg" : "PIN Incorrect",
+  "apiKeyAccepted" : true,
+  "pinAccepted" : false
+}
+
+
 
 ## Authenticated User Actions
 
-GET /user/:uID
+### GET /user/:uID
 
 Retrieves all relevant user data
 
 submission example:
+
+```javascript
 {
   "accessToken" : <accessToken>
 }
+```
 
 reply Example:
 
+```javascript
 {
   "uID" : "1403378",
   "uName" : "ljs34",
@@ -44,3 +117,4 @@ reply Example:
   "isWeekly": false,
   "bonusBucks" : 15.44  
 }
+```
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var express = require('express')
 var app = express()
 var bodyParser = require ("body-parser")
-
+const uuidV1 = require('uuid/v1')
+var MongoClient = require('mongodb').MongoClient
+// Connection url
+const mongodb_url = 'mongodb://localhost:27017/kiosk';
+var winston = require('winston')
+winston.add(winston.transports.File, {filename : "/var/log/kiosk/api.log"})
+winston.level = "debug"
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -10,28 +16,56 @@ app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 
 app.get('/', function(request, response) {
-	console.log("Connection accepted from " + request.ip)
+	winston.info("Connection accepted from " + request.ip)
   response.send('Hello, user! Your IP address is ' + request.ip)
 })
 
 app.get('/test', function(request, response) {
-	console.log("Test requested from client at " + request.ip)
+	winston.info("Test requested from client at " + request.ip)
 	response.send('Test server running')
 })
 
 app.get('/user/:uID', function(req, res) {
+	// send a 400 error if there isn't any user data uploaded, like auth key
 	if (!req.body) return res.sendStatus(400)
-	console.log("Attempting to get data from " + req.params.uID)
+	winston.info("Attempting to get data from " + req.params.uID)
 	res.setHeader('Content-Type', 'application/json');
-	res.json({"uID" : req.params.uID, "username" : "Testy"})
+	res.status(201).json({"uID" : req.params.uID, "username" : "Testy"})
+})
+
+app.post('/login/:uID', function(req, res) {
+	// check to make sure LOGIN action succeeds
+
+	// create an accessToken to register to the account
+	var accessToken = uuidV1();
+	// create access token reference in database
+
+
+	// Connect using MongoClient
+	MongoClient.connect(mongodb_url, function(err, db) {
+		// Create a collection we want to drop later
+		var col = db.collection('access_token');
+		// add the access token to the database
+		col.insertOne({"accessToken" : accessToken, "uID" : req.params.uID, "created" : new Date()})
+		// // Show that duplicate records got dropped
+		// col.find({}).toArray(function(err, items) {
+		//   test.equal(null, err);
+		//   test.equal(4, items.length);
+		//   db.close();
+		// });
+	});
+
+	res.setHeader('Content-Type', 'application/json');
+	res.json({"uID" : req.params.uID, "accessToken" : accessToken})
+
 })
 
 app.get('/*', function(request, response) {
-	console.log("Unknown request: " + request.path)
+	winston.warn("Unknown request: " + request.path)
 	//console.log("Weird request coming in on " + request.route)
-	response.status(404).send('404 Error: path ' + request.path + ' is not a valid path')
+	response.status(404).send('404 Error: path ' + request.path + ' is not a valid path\n')
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  winston.debug("Node app is running at localhost:" + app.get('port'))
 })
