b07d91db990e8834d4fd8f573d9fee67dd3c28f2
Landon Sterk <landonsterk@gmail.com>
2017-03-15 16:28:51 -0400

    New scripts and gitignore test_auth will run POST /login/132 and test_user will run GET /user/132

---
+++ test_auth.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -X POST -H "Content-Type: application/json" -d '{"PIN":"1234", "apiKey":"happy"}' http://localhost:5000/login/132
+
---
+++ test_user.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl -X GET -H "Content-Type: application/json" -d '{"accessToken" : "1234-5678-90ab-cdef"}' http://localhost:5000/user/132
